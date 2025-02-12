930a6a9daceae039644ac8d86cffec21c6c8e032
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-02-11 22:34:35 -0500

    testing image and basic structure to determine order and main content

--- data/pokemons.js
+++ data/pokemons.js
@@ -1,8 +1,8 @@
 let pokemons = [
-    {name: "charmender", imgHidden: "C:\Users\Juan\Desktop\JS-Projects\Intro-Projects\Guess-Pokemon-Fan-App\assets\8ccf2679378a0eedef2d2369739ce4be.jpg", img: ""},
+    {name: "charmender", imgHidden: "", img: ""},
     {name: "pidgey", imgHidden:"", img: ""},
     {name: "dratini", imgHidden: "", img: ""},
-    {name: "zapdos", imgHidden: "", img: ""}
+    {name: "zapdos", imgHidden: "", img: ""},
     {name: "scyther", imgHidden: "", img: ""},
     {name: "pikachu", imgHidden: "", img: ""},
     {name: "gastly", imgHidden: "", img: ""},
