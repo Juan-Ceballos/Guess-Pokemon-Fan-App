74ac6983d60d8d30614da8ffd4a861c1479c517c
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-02-11 21:43:33 -0500

    added pokemon data form of array of literal objects

---
+++ data/pokemons.js
@@ -0,0 +1,13 @@
+let pokemons = [
+    {name: "charmender", imgHidden: "C:\Users\Juan\Desktop\JS-Projects\Intro-Projects\Guess-Pokemon-Fan-App\assets\8ccf2679378a0eedef2d2369739ce4be.jpg", img: ""},
+    {name: "pidgey", imgHidden:"", img: ""},
+    {name: "dratini", imgHidden: "", img: ""},
+    {name: "zapdos", imgHidden: "", img: ""}
+    {name: "scyther", imgHidden: "", img: ""},
+    {name: "pikachu", imgHidden: "", img: ""},
+    {name: "gastly", imgHidden: "", img: ""},
+    {name: "sandshrew", imgHidden: "", img: ""},
+    {name: "squirtle", imgHidden: "", img: ""},
+    {name: "caterpie", imgHidden: "", img: ""},
+    {name: "ratata", imgHidden: "", img: ""},
+]
