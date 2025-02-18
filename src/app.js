bb3748f5d78f234bf7e19d096ef0b29eb09974e3
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-02-17 20:25:00 -0500

    added listener to input

--- src/app.js
+++ src/app.js
@@ -4,3 +4,26 @@ console.log(h1.innerText)
 
 const gameSpace = document.getElementById("result")
 console.log(gameSpace.innerText)
+
+let guessForm = document.querySelector("form")
+let input = guessForm.firstElementChild
+
+console.log(guessForm)
+console.log(input)
+
+
+
+
+function addFormEventListener() {
+    let guessForm = document.querySelector("form")
+    let inputField = guessForm.firstElementChild
+    inputField.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault()
+            let enteredText = inputField.value
+            console.log(enteredText)
+        }
+    })
+}
+
+addFormEventListener()
